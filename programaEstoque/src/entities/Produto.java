3613d7fe29a3a94a7b3307dc1fe3d1fb79a03c45
anageorgia <anageorgiagp@gmail.com>
2020-12-19 18:57:57 -0300

    adicionando getters e setters

--- programaEstoque/src/entities/Produto.java
+++ programaEstoque/src/entities/Produto.java
@@ -45,4 +45,25 @@ public class Produto {
             + " unidades, Total: R$ "
             + String.format("%.2f", valorTotalEmEstoque());
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
 }
